077456e491423a073bfc04c23518453851fbd9b4
TeaTurjak <tea.turjak@gmail.com>
2022-06-10 14:28:41 +0200

    First commit: App commit

---
+++ app/src/main/java/com/example/dayplanner/LogIn.kt
@@ -0,0 +1,18 @@
+package com.example.dayplanner
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val actionBar: ActionBar?
+        actionBar = supportActionBar
+        val colorDrawable = ColorDrawable(Color.parseColor("#CE88A5"))
+        actionBar!!.setBackgroundDrawable(colorDrawable)
+        setContentView(R.layout.activity_homepage)
+    }
+}
